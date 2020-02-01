883b2a308b8adb98777b22cbf98b2bcec1b101a8
Nicholas Woo <nickj_woo@yahoo.com>
2020-02-01 14:14:56 -0800

    Updates to back end, started on scripts

--- templates/script.js
+++ templates/script.js
@@ -1,17 +1,16 @@
-function clicked(){
-    data = {"term": "food","location": "New York City"}
-    let url = "https://api.yelp.com/v3/businesses/search?";
-    fetch(url, {
-        method: "POST",
+
+async function formSubmit(event, food, location){
+    console.log(String(food));
+    event.preventDefault();
+    let proxy_url = "https://cors-anywhere.herokuapp.com/";
+    let url = `https://api.yelp.com/v3/businesses/search?term={$food}&location={$location}`;
+    let response = await fetch (proxy_url + url,{
+        method: 'GET',
         withCredentials: true,
         headers: {
-            "Authorization": "Bearer iqJ5W-M7CR9UGd-iRcSSUk3epS3bnolj3jGlcN5ENRuxvUvnYtTt-w_Oe-iMH2lyVMP0_6J1EHXz7Gng1cQDn4XFIEJfy4jmsh_BCkm_zUQhSYaYT06Z4_pvkfs0XnYx",
-            "Content-Type": "application/json"
+            Authorization: 'Bearer iqJ5W-M7CR9UGd-iRcSSUk3epS3bnolj3jGlcN5ENRuxvUvnYtTt-w_Oe-iMH2lyVMP0_6J1EHXz7Gng1cQDn4XFIEJfy4jmsh_BCkm_zUQhSYaYT06Z4_pvkfs0XnYx'
         },
-        body: JSON.stringify(data)
-    })
-    .then(response => {console.log(response);return response.json();})
-    .then(data => {
-        console.log(JSON.parse(data));
     });
-}
+    console.log(response);
+    let result = await response.json();
+    console.log(result);    
