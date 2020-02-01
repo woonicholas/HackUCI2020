4c5e8427f69171cb8968fd8f435a3d5e6e9c489f
woonicholas <nickj_woo@yahoo.com>
2020-02-01 13:06:08 -0800

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -4,7 +4,6 @@ function clicked(){
     fetch(url, {
         method: "POST",
         withCredentials: true,
-        mode : "no-cors",
         headers: {
             "Authorization": "Bearer iqJ5W-M7CR9UGd-iRcSSUk3epS3bnolj3jGlcN5ENRuxvUvnYtTt-w_Oe-iMH2lyVMP0_6J1EHXz7Gng1cQDn4XFIEJfy4jmsh_BCkm_zUQhSYaYT06Z4_pvkfs0XnYx",
             "Content-Type": "application/json"
