1586d6a1259ee3328d11634f59854cb8330b7df4
NguyenBran <nguyen.brandon.0526@gmail.com>
2020-02-01 11:55:59 -0800

    backend start :(

---
+++ templates/script.js
@@ -0,0 +1,18 @@
+function clicked(){
+    data = {"term": "food","location": "New York City"}
+    let url = "https://api.yelp.com/v3/businesses/search?";
+    fetch(url, {
+        method: "POST",
+        withCredentials: true,
+        mode : "no-cors",
+        headers: {
+            "Authorization": "Bearer iqJ5W-M7CR9UGd-iRcSSUk3epS3bnolj3jGlcN5ENRuxvUvnYtTt-w_Oe-iMH2lyVMP0_6J1EHXz7Gng1cQDn4XFIEJfy4jmsh_BCkm_zUQhSYaYT06Z4_pvkfs0XnYx",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {console.log(response);return response.json();})
+    .then(data => {
+        console.log(JSON.parse(data));
+    });
+}
