4a524a736a9975078a69fab587defa31c3ffc32c
Nicholas Woo <nickj_woo@yahoo.com>
2020-02-01 15:18:47 -0800

    implement location selector

--- templates/script.js
+++ templates/script.js
@@ -11,7 +11,15 @@ async function formSubmit(event, food, location){
             Authorization: 'Bearer iqJ5W-M7CR9UGd-iRcSSUk3epS3bnolj3jGlcN5ENRuxvUvnYtTt-w_Oe-iMH2lyVMP0_6J1EHXz7Gng1cQDn4XFIEJfy4jmsh_BCkm_zUQhSYaYT06Z4_pvkfs0XnYx'
         },
     });
-    console.log(response);
     let result = await response.json();
     console.log(result); 
-} 
+    selectLocation(result);
+    return result;
+}
+
+//parse JSON code
+function selectLocation(result){
+    console.log(result.businesses[0]);
+    document.getElementById('1').innerHTML = result.businesses[0].name;
+    document.getElementById('pic1').src = result.businesses[0].image_url;
+}
